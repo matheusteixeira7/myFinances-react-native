005b2b6267165140e70cab8ec9d59885660be90c
matheusteixeira7 <matheus.teixeira7@gmail.com>
2022-01-20 14:25:16 -0300

    chore: add dumb transaction data

---
+++ src/utils/transactions.ts
@@ -0,0 +1,29 @@
+export const data = [
+  {
+    id: '1',
+    amount: 'R$ 60,00',
+    category: 'Food',
+    date: '2020-01-01',
+    icon: 'coffee',
+    title: 'Pizza',
+    type: 'outgoing'
+  },
+  {
+    id: '2',
+    amount: 'R$ 10.000,00',
+    category: 'Freelance',
+    date: '2020-01-01',
+    icon: 'dollar-sign',
+    title: 'Desenvolvimento de app',
+    type: 'incoming'
+  },
+  {
+    id: '3',
+    amount: 'R$ 20,00',
+    category: 'Food',
+    date: '2020-01-01',
+    icon: 'dollar-sign',
+    title: 'Almoço',
+    type: 'outgoing'
+  }
+]
